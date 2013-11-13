99da9d8c2619c1ec43700b1913bd90c75360f8f9
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2013-11-13 21:57:59 +0900

    Initial Commit

---
+++ Qiitarian/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Qiitarian
+//
+//  Created by Shoichi Matsuda on 2013/11/13.
+//  Copyright (c) 2013年 Shoichi Matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Qiitarian/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Qiitarian
+//
+//  Created by Shoichi Matsuda on 2013/11/13.
+//  Copyright (c) 2013年 Shoichi Matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
