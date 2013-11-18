fe8cdfcebb6a69660fa21ff0fd5ad6b712033ee6
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2013-11-18 21:37:47 +0900

    ViewController用のクラスを作成

---
+++ Qiitarian/latest/QiitarianLatestViewController.h
@@ -0,0 +1,13 @@
+//
+//  QiitarianLatestViewController.h
+//  Qiitarian
+//
+//  Created by Shoichi Matsuda on 2013/11/18.
+//  Copyright (c) 2013年 Shoichi Matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QiitarianLatestViewController : UIViewController
+
+@end
