318e9bbe3ce6cd1ffec9e620f5809681b5f7ce06
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2013-11-22 15:46:51 +0900

    URLへのアクセス用処理用のクラスを作成

---
+++ Qiitarian/latest/model/QiitarianLatestItemsFetcher.h
@@ -0,0 +1,13 @@
+//
+//  QiitarianLatestItemsFetcher.h
+//  Qiitarian
+//
+//  Created by Shoichi Matsuda on 2013/11/22.
+//  Copyright (c) 2013年 Shoichi Matsuda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QiitarianLatestItemsFetcher : NSObject
+
+@end
---
+++ Qiitarian/latest/model/QiitarianLatestItemsFetcher.m
@@ -0,0 +1,13 @@
+//
+//  QiitarianLatestItemsFetcher.m
+//  Qiitarian
+//
+//  Created by Shoichi Matsuda on 2013/11/22.
+//  Copyright (c) 2013年 Shoichi Matsuda. All rights reserved.
+//
+
+#import "QiitarianLatestItemsFetcher.h"
+
+@implementation QiitarianLatestItemsFetcher
+
+@end
