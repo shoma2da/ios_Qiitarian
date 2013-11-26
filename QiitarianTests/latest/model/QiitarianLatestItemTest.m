9eab43a19b4eb8975097a04cb25c51be80cbb27b
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2013-11-26 18:25:01 +0900

    テストクラスを追加

---
+++ QiitarianTests/latest/model/QiitarianLatestItemTest.m
@@ -0,0 +1,34 @@
+//
+//  QiitarianLatestItemTest.m
+//  Qiitarian
+//
+//  Created by Shoichi Matsuda on 2013/11/26.
+//  Copyright (c) 2013年 Shoichi Matsuda. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QiitarianLatestItemTest : XCTestCase
+
+@end
+
+@implementation QiitarianLatestItemTest
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
