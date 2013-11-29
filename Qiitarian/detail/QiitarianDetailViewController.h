8f2df56a2fa76f0c700ccf4ef3f5a1a391d530ec
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2013-11-29 23:35:04 +0900

    ViewControllerをクラスと結びつけ

---
+++ Qiitarian/detail/QiitarianDetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  QiitarianDetailViewController.h
+//  Qiitarian
+//
+//  Created by Shoichi Matsuda on 2013/11/29.
+//  Copyright (c) 2013年 Shoichi Matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QiitarianDetailViewController : UIViewController
+
+@property (nonatomic) NSString *uuid;
+
+@end
